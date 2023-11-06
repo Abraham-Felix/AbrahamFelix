f822c6f7eb7e0fd4d9c7b4d91b99af3f0ce972f9
abraham felix <apesyntax@gmail.com>
2023-11-05 20:02:01 -0700

    Sidebar basic functionality 👨‍🔧 + Home Lik

--- AbrahamFelix/src/layouts/default/Default.vue
+++ AbrahamFelix/src/layouts/default/Default.vue
@@ -1,8 +1,16 @@
 <template>
   <v-layout class="rounded rounded-md">
 
-    <v-app-bar :elevation="3" :color="'green'" :order="order" color="grey-lighten-2" flat title="Application bar"
-      location="top" density="comfortable">
+    <v-app-bar :elevation="3" :color="'green'" :order="order" color="grey-lighten-2" flat location="top"
+      density="comfortable">
+
+      <template #prepend>
+        <v-toolbar-title>
+          <router-link class="text-decoration-none text-white" to="/">
+            Home
+          </router-link>
+        </v-toolbar-title>
+      </template>
       <!-- <template v-slot:append>
         <v-switch v-model="order" hide-details inset label="Toggle order" true-value="-1" false-value="0"></v-switch>
       </template> -->
@@ -25,7 +33,18 @@
 
     </v-app-bar>
 
-    <v-navigation-drawer color="grey-darken-2"></v-navigation-drawer>
+    <v-navigation-drawer permanent expand-on-hover rail color="grey-darken-2">
+      <v-list>
+        <router-link class="text-decoration-none text-white" to="posts">
+          <v-list-item to="posts" prepend-icon="mdi-book" value="posts" title="Posts">
+          </v-list-item>
+        </router-link>
+        <router-link class="text-decoration-none text-white" to="comments">
+          <v-list-item prepend-icon="mdi-comment" value="comments" title="Comments">
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
       <router-view />
